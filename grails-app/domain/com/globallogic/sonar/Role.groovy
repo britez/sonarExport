90d2c4c6470a3b9675b7f0f98d2ffef1b4733f53
Maximiliano Britez <maximiliano.britez@globallogic.com>
2013-03-12 11:24:34 -0300

    First commit

---
+++ grails-app/domain/com/globallogic/sonar/Role.groovy
@@ -0,0 +1,14 @@
+package com.globallogic.sonar
+
+class Role {
+
+	String authority
+
+	static mapping = {
+		cache true
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+}
---
+++ grails-app/domain/com/globallogic/sonar/User.groovy
@@ -0,0 +1,47 @@
+package com.globallogic.sonar
+
+/**
+ * Clase de seguridad, para obtener el acceso a la aplicación.
+ * Además aloja las credenciales de google drive.
+ * 
+ * @author maximiliano.britez
+ */
+class User {
+
+	transient springSecurityService
+
+	String username
+	String password
+	boolean enabled
+	boolean accountExpired
+	boolean accountLocked
+	boolean passwordExpired
+
+	static constraints = {
+		username blank: false, unique: true
+		password blank: false
+	}
+
+	static mapping = {
+		password column: '`password`'
+	}
+
+	Set<Role> getAuthorities() {
+		UserRole.findAllByUser(this).collect { it.role } as Set
+	}
+
+	def beforeInsert() {
+		encodePassword()
+	}
+
+	def beforeUpdate() {
+		if (isDirty('password')) {
+			encodePassword()
+		}
+	}
+
+	/** Se encodea el password antes de persistir */
+	protected void encodePassword() {
+		password = springSecurityService.encodePassword(password)
+	}
+}
