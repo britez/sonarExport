4df0af1fe244a166aefd9b8b62c0b5e982bb1f28
Maximiliano Britez <maximiliano.britez@globallogic.com>
2013-03-25 13:47:01 -0300

    Clean bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -7,14 +7,6 @@ import com.globallogic.sonar.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-		
-		def adminRole = new Role(authority:"ROLE_ADMIN").save(failOnError: true)
-		def user = new User(username:"maxi", password: "maxi").save(failOnError: true)
-		def googleCredentials = new GoogleCredentials(username: "maxi03.15", password: "NarelaYael88*", user: user).save(failOnError: true)
-		def sonarEnvirenment = new SonarEnvironment(serverUrl: "http://sonar.analytical-labs.com", projectKey: "org.saiku:saiku-webapp-mondrian4", user: user).save(failOnError: true)
-		
-		UserRole.create user, adminRole
-		
     }
     def destroy = {
     }
